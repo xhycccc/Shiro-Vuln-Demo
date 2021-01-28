5374968199f6404a7fc78e188b0b4e50b15908a2
xinhaiyucheng <1726401743@qq.com>
2021-01-28 17:26:05 +0800

    add shiro_cve-2020-11989

---
+++ shiro_cve-2020-11989/src/test/java/org/syclover/srpingbootshiro/SrpingbootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syclover.srpingbootshiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
